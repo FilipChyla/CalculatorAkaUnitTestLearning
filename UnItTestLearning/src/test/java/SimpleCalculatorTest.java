42cd20ac8e19807c335c990b31f8ed0f67484261
FilipChyla <96245628+FilipChyla@users.noreply.github.com>
2022-03-06 18:03:20 +0100

    Add files via upload

--- UnItTestLearning/src/test/java/SimpleCalculatorTest.java
+++ UnItTestLearning/src/test/java/SimpleCalculatorTest.java
@@ -1,82 +1,52 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.Random;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class SimpleCalculatorTest {
+    @Test
+    void twoNumbersAddedShouldReturnCorrectResult(){
+        SimpleCalculator simpleCalculator=new SimpleCalculator();
 
-    @Test
-    void twoPlusTwoShouldEqualFour(){
-        SimpleCalculator simpleCalculator=new SimpleCalculator();
-        assertEquals(4, simpleCalculator.add(2,2));
-    }
-    @Test
-    void threePlusTenShouldEqualThirteen(){
-        SimpleCalculator simpleCalculator=new SimpleCalculator();
-        assertEquals(13, simpleCalculator.add(3,10));
-    }
-    @Test
-    void tenPlusThreeShouldEqualThirteen(){
-        SimpleCalculator simpleCalculator=new SimpleCalculator();
-        assertEquals(13, simpleCalculator.add(10,3));
+        assertThat(simpleCalculator.add(1,3)).isEqualTo(4);
+        assertThat(simpleCalculator.add(0,8)).isEqualTo(8);
+        assertThat(simpleCalculator.add(3,15)).isEqualTo(18);
+        assertThat(simpleCalculator.add(7,9)).isEqualTo(16);
+        assertThat(simpleCalculator.add(-30.5f,-0.2f)).isEqualTo(-30.7f);
     }
 
     @Test
-    void twoMinusTwoShouldEqualZero(){
+    void twoNumbersSubtractedShouldReturnCorrectResult(){
         SimpleCalculator simpleCalculator=new SimpleCalculator();
-        assertEquals(0, simpleCalculator.subtract(2,2));
-    }
-    @Test
-    void threeMinusTenShouldEqualNegativeSeven(){
-        SimpleCalculator simpleCalculator=new SimpleCalculator();
-        assertEquals(-7, simpleCalculator.subtract(3,10));
-    }
-    @Test
-    void tenMinusThreeShouldEqualSeven(){
-        SimpleCalculator simpleCalculator=new SimpleCalculator();
-        assertEquals(7, simpleCalculator.subtract(10,3));
-    }
-    @Test
-    void anythingMinusSameNumberShouldEqualZero(){
-        SimpleCalculator simpleCalculator=new SimpleCalculator();
-        assertEquals(0, simpleCalculator.subtract(8,8));
+
+        assertThat(simpleCalculator.subtract(5,3)).isEqualTo(2);
+        assertThat(simpleCalculator.subtract(8,12)).isEqualTo(-4);
+        assertThat(simpleCalculator.subtract(7.2f,4.6f)).isEqualTo(2.6f);
+        assertThat(simpleCalculator.subtract(12,-8)).isEqualTo(20);
+        assertThat(simpleCalculator.subtract(-6.9f,-53.5f)).isEqualTo(46.6f);
     }
 
     @Test
-    void twoTimesTwoShouldEqualFour(){
+    void twoNumbersMultipliedShouldReturnCorrectResult(){
         SimpleCalculator simpleCalculator=new SimpleCalculator();
-        assertEquals(4, simpleCalculator.multiply(2,2));
-    }
-    @Test
-    void threeTimesTenShouldEqualThirty(){
-        SimpleCalculator simpleCalculator=new SimpleCalculator();
-        assertEquals(30, simpleCalculator.multiply(3,10));
-    }
-    @Test
-    void tenTimesThreeShouldEqualThirty(){
-        SimpleCalculator simpleCalculator=new SimpleCalculator();
-        assertEquals(30, simpleCalculator.multiply(10,3));
-    }
-    @Test
-    void anythingTimesZeroShouldEqualZero(){
-        SimpleCalculator simpleCalculator=new SimpleCalculator();
-        assertEquals(0, simpleCalculator.multiply(6,0));
+
+        assertThat(simpleCalculator.multiply(3,4)).isEqualTo(12);
+        assertThat(simpleCalculator.multiply(5,8)).isEqualTo(40);
+        assertThat(simpleCalculator.multiply(-3,6)).isEqualTo(-18);
+        assertThat(simpleCalculator.multiply(-2,-3.5f)).isEqualTo(7);
+        assertThat(simpleCalculator.multiply(-0.5f, -12.5f)).isEqualTo(6.25f);
     }
 
     @Test
-    void twoDividedByTwoShouldEqualOne(){
+    void twoNumbersDividedShouldReturnCorrectResult() throws Exception {
         SimpleCalculator simpleCalculator=new SimpleCalculator();
-        assertEquals(1,simpleCalculator.divide(2,2));
-    }
-    @Test
-    void threeDividedBySixShouldEqualHalf(){
-        SimpleCalculator simpleCalculator=new SimpleCalculator();
-        assertEquals(0.5,simpleCalculator.divide(3,6));
-    }
-    @Test
-    void fifteenDividedByFiveShouldEqualThree(){
-        SimpleCalculator simpleCalculator=new SimpleCalculator();
-        assertEquals(3,simpleCalculator.divide(15,5));
+
+        assertThat(simpleCalculator.divide(4,2)).isEqualTo(2);
+        assertThat(simpleCalculator.divide(21,3)).isEqualTo(7);
+        assertThat(simpleCalculator.divide(10,4)).isEqualTo(2.5f);
+        assertThat(simpleCalculator.divide(-6,-2)).isEqualTo(3);
+        assertThat(simpleCalculator.divide(-1.5f,0.5f)).isEqualTo(-3);
+
+        Throwable thrown = catchThrowable(() -> simpleCalculator.divide(8,0));
+        assertThat(thrown).isInstanceOf(Exception.class);
     }
 }
